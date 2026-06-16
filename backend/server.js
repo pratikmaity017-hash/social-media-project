4810d9187fede041b453856961ee0cd88cf50751
pratikmaity017-hash <pratikmaity017@gmail.com>
2026-06-16 15:09:53 +0530

    fix: correct import statement formatting and update app.listen parameters

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 // server run korlam
 
 import app from "./src/app.js";
-import connectDB from './src/config/db.js'
+import connectDB from "./src/config/db.js";
 
-connectDB()
+connectDB();
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("server is running on port 3000");
 });
