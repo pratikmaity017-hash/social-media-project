a2e3129c858b0e573d2090fb5a48d8f9abeb0b40
pratikmaity017-hash <pratikmaity017@gmail.com>
2026-06-18 01:00:54 +0530

    fix: reorder middleware initialization for express.json and cookieParser

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,14 +9,14 @@ import notificationRouter from "./routes/notification.route.js";
 import userRouter from "./routes/users.route.js";
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
